b15bf8cf19d305dc9328cd2b773b7bc2f9462d9c
Your Name <you@example.com>
2021-09-12 22:20:02 +0300

    add main section

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './index.scss';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+
+
+function App() {
+  return (
+    <>
+    <Product/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import styled from 'styled-components'
+import Navbar from '../components/Navbar'
+import Products from '../components/Products'
+import Announcement from '../components/Announcement'
+import NewLetter from '../components/NewLetter'
+import Footer from '../components/Footer'
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``
+
+const ProductList = () => {
+    return (
+        <Container>
+            <Navbar/>
+            <Announcement/>
+            <Title>Dresses</Title>
+            <FilterContainer>
+              <Filter><FilterText>Filter Products:</FilterText>
+              <Select>
+              <Option disabled selected>Color</Option>
+              <Option>White</Option>
+              <Option>Black</Option>
+              <Option>Red</Option>
+              <Option>Blue</Option>
+              <Option>Yellow</Option>
+              <Option>Green</Option>
+              </Select>
+              <Select>
+            <Option disabled selected>Size</Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+            </Select>
+              </Filter>
+              <Filter><FilterText>Sort Products:</FilterText></Filter>
+              <Select style={{margin:'20px'}}>
+            <Option>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+              </Select>
+            </FilterContainer>
+            <Products/>
+            <NewLetter/>
+            <Footer/>
+        </Container>
+    )
+}
+
+export default ProductList
